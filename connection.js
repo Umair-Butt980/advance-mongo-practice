a1027be7d6a2686c155296b02fe6834aa571bf13
Umair Butt <umairamjadbutt1@gmail.com>
2023-11-30 00:02:30 +0500

    new main function for the connection

--- connection.js
+++ connection.js
@@ -1,12 +1,22 @@
 const {MongoClient} = require('mongodb');
 require('dotenv').config()
 
+const { URI } = process.env;
+console.log(URI)
 
-try {
-    const { URI } = process.env;
+async function main(){
     const client = new MongoClient(URI);
-    console.log(client , 'Connected to MongoDB');    
-} catch (error) {
-    console.log(error, 'There was an error while connecting to the mongo DB');
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await listDatabases(client);
+        console.log(dbList , '<====This is the DB list=====>')
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
 }
-
+main().catch(console.error);
